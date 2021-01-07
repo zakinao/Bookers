2d354a89e1eff84dc00b6a481785ea81bcec47f0
zakinao <zakinaororo@gmail.com>
2021-01-07 09:30:49 +0000

    Finish bookers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,54 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all.order(:id)
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice]="Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all.order(:id)
+      render "index"
+    end
+  end
+
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice]= "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice]="Book was successfully destroyed."
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
